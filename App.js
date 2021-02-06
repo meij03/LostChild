46a9e9b19dd8f44954c0a60fca33aece80346399
meij03 <jm7787@nyu.edu>
2021-02-06 12:18:26 -0500

    TestingReact and install bootstrap

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//import "bootstrap/dist/css/bootstrap.css"
 
+let x = 1;
+console.log("Hello World1");
 export default function App() {
   return (
     <View style={styles.container}>
