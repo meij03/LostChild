8212cff3bb3fb4ff7a9a82090349954e1d6593b3
meij03 <jm7787@nyu.edu>
2021-02-07 00:59:08 -0500

    Making branch known

--- App.js
+++ App.js
@@ -18,13 +18,14 @@ export default function App() {
       </stack.Navigator>
     </NavigationContainer>
   );
+}
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
